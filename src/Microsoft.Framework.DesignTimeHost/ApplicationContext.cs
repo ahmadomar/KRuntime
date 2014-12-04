f137d16058ce437604e9ea55db8dedbec41f9834
David Fowler <davidfowl@gmail.com>
2014-12-03 18:30:30 -0800

    Made it work

--- src/Microsoft.Framework.DesignTimeHost/ApplicationContext.cs
+++ src/Microsoft.Framework.DesignTimeHost/ApplicationContext.cs
@@ -826,18 +826,6 @@ namespace Microsoft.Framework.DesignTimeHost
         {
             var cacheKey = Tuple.Create("ApplicationContext", project.Name, configuration, frameworkName);
 
-            IAssemblyLoadContextFactory loadContextFactory = null;
-
-            if (useRuntimeLoadContextFactory)
-            {
-                var runtimeApplicationContext = GetApplicationHostContext(project,
-                                                                          _appEnv.Configuration,
-                                                                          _appEnv.RuntimeFramework,
-                                                                          useRuntimeLoadContextFactory: false);
-
-                loadContextFactory = new AssemblyLoadContextFactory(runtimeApplicationContext.ServiceProvider);
-            }
-
             return _cache.Get<ApplicationHostContext>(cacheKey, ctx =>
             {
                 var applicationHostContext = new ApplicationHostContext(_hostServices,
@@ -848,7 +836,7 @@ namespace Microsoft.Framework.DesignTimeHost
                                                                         cache: _cache,
                                                                         cacheContextAccessor: _cacheContextAccessor,
                                                                         namedCacheDependencyProvider: _namedDependencyProvider,
-                                                                        loadContextFactory: loadContextFactory);
+                                                                        loadContextFactory: GetRuntimeLoadContextFactory(project));
 
                 applicationHostContext.DependencyWalker.Walk(project.Name, project.Version, frameworkName);
 
@@ -866,6 +854,22 @@ namespace Microsoft.Framework.DesignTimeHost
 
                 return applicationHostContext;
             });
+        }
+
+        private IAssemblyLoadContextFactory GetRuntimeLoadContextFactory(Project project)
+        {
+            var applicationHostContext = new ApplicationHostContext(_hostServices,
+                                                                    project.ProjectDirectory,
+                                                                    packagesDirectory: null,
+                                                                    configuration: _appEnv.Configuration,
+                                                                    targetFramework: _appEnv.RuntimeFramework,
+                                                                    cache: _cache,
+                                                                    cacheContextAccessor: _cacheContextAccessor,
+                                                                    namedCacheDependencyProvider: _namedDependencyProvider);
+
+            applicationHostContext.DependencyWalker.Walk(project.Name, project.Version, _appEnv.RuntimeFramework);
+
+            return new AssemblyLoadContextFactory(applicationHostContext.ServiceProvider);
         }
 
         private DependencyInfo ResolveProjectDepencies(Project project, string configuration, FrameworkName frameworkName)
